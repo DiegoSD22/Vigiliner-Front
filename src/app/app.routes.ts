d15f03eb145a013ebc0df9d499dd3ff72fce43d2
Alejandro Gómez <alejandrogomez2339@gmail.com>
2026-02-18 16:26:38 -0500

    cascaron table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard'
+import { VigiTableComponent } from './shared/design-system/vigi-table/vigi-table.component';
 
 export const routes: Routes = [
-    {path: '', component: Dashboard}
+    {path: '', component: Dashboard},
+    {path: 'table', component: VigiTableComponent}
 ];
---
+++ src/app/shared/design-system/vigi-table/vigi-table.component.ts
@@ -0,0 +1,11 @@
+// Componente base para VigiTable
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vigi-table',
+  templateUrl: './vigi-table.component.html',
+  styleUrls: ['./vigi-table.component.css']
+})
+export class VigiTableComponent {
+  // Lógica del componente
+}
